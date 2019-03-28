ef971babdd75f7c7db6bf43488411b56a750ddb1
unknown <nil_al_7@hotmail.com>
2019-03-28 00:38:18 -0500

    newton interpolation

--- interpolation_1/newton_divided_differences.py
+++ interpolation_1/newton_divided_differences.py
@@ -9,15 +9,29 @@ def newpoly(X,Y):
 
     n = len(X)
     D = np.zeros((n,n))
-    D[:,1] = np.tranpose(Y)
+    D[:,0] = np.transpose(Y)
 
-    for j in range(2, n):
+    for j in range(1, n):
         for k in range(j, n):
-            D[k,j] = (D[k,j-1]-D[k-1,j-1])/(X[k]-X[k-j+1])
+            D[k,j] = (D[k,j-1]-D[k-1,j-1])/(X[k]-X[k-j])
 
-    C = D[n,n]
+    C = D[n-1,n-1]
+
+    for k in range(n-1-1,-1,-1):
+        C = np.convolve(C,np.poly(X[k]))
+        m = len(C) - 1 #cuz in python array starts at 0
+        C[m] +=  D[k,k]
+
+    return C,D
 
 def main():
 
+    #X'
+    X = [0, 2, 3, 5, 6, 1]
+    #Y' (x^3)
+    Y = [0, 8, 27, 125, 216, 1]
+    R = newpoly(X,Y)
+    print("C: \n",R[0])
+    print("D: \n",R[1])
 
 main()
