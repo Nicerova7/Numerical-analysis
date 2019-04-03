746c166504a54ac29e58b9d60b29c866a76ce1db
unknown <nil_al_7@hotmail.com>
2019-04-03 00:24:54 -0500

    Bisection method

--- sol_nonlinear_eq_0/bisection.py
+++ sol_nonlinear_eq_0/bisection.py
@@ -11,21 +11,21 @@ def bisection(f,a,b):
         else:
             return False
 
-        c = (a+b)/2.0
-        nextt = f(c)
-        if (close_enough(nextt,0.0)):
-            return c
-        else:
-            fa = f(a)
-            fb = f(b)
-            fc = f(c)
-            if (fa*fc<0):
-                return (bisection(f,a,c))
-            if (fc*fb<0):
-                return (bisection(f,c,b))
+    c = (a+b)/2.0
+    nextt = f(c)
+    if (close_enough(nextt,0.0)):
+        return c
+    else:
+        fa = f(a)
+        fb = f(b)
+        fc = f(c)
+        if (fa*fc<0):
+            return (bisection(f,a,c))
+        if (fc*fb<0):
+            return (bisection(f,c,b))
 
 def func(x):
     z = x**4-2*x**3-4*x**2+4*x+4
     return z
 
-print(bisection(func,-2,-1)
+print(bisection(func,-2,-1))
