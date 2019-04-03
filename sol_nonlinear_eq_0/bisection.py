2a4f5a05595d639736f0c8483a7285b4be93b26c
unknown <nil_al_7@hotmail.com>
2019-04-02 23:51:37 -0500

    Solution of nonlinear equation

---
+++ sol_nonlinear_eq_0/bisection.py
@@ -0,0 +1,31 @@
+# Bisection
+import numpy as np
+
+tolerance = 0.000000001
+
+def bisection(f,a,b):
+
+    def close_enough(v1,v2):
+        if(np.abs(v1-v2)<tolerance):
+            return True
+        else:
+            return False
+
+        c = (a+b)/2.0
+        nextt = f(c)
+        if (close_enough(nextt,0.0)):
+            return c
+        else:
+            fa = f(a)
+            fb = f(b)
+            fc = f(c)
+            if (fa*fc<0):
+                return (bisection(f,a,c))
+            if (fc*fb<0):
+                return (bisection(f,c,b))
+
+def func(x):
+    z = x**4-2*x**3-4*x**2+4*x+4
+    return z
+
+print(bisection(func,-2,-1)
