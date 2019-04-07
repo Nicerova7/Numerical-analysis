5b404cbaed037ba6f3bcd6cb64d7941d495ff7c8
unknown <nil_al_7@hotmail.com>
2019-04-06 20:56:40 -0500

    simpson 3/8

---
+++ integration_2/NewtonCotes/simpson38.py
@@ -0,0 +1,20 @@
+#Simpson 3/8 rule
+import numpy as np
+
+def simpson(f,a,b,n):
+
+    h = (b-a)*1.0/n
+    x = np.linspace(a,b,n+1)
+    C = np.ones([n+1,1])     
+    C[1:n-1:3] = 3
+    C[2:n:3] = 3
+    C[3:n-2:3] = 2
+    z = (3*h/8)*np.dot(f(x),C)
+    return z
+
+def func(x):
+
+    z = x**2 #
+    return z
+
+print(simpson(func,1,2,100))
