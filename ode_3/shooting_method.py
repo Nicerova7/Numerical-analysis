3a0ed3d4de454257b5f4394edaff2fa17e44f82f
unknown <nil_al_7@hotmail.com>
2019-06-02 02:10:15 -0500

    Shooting method

--- ode_3/shooting_method.py
+++ ode_3/shooting_method.py
@@ -10,14 +10,16 @@
 """
 
 import numpy as np
+import rungekutta4-system as rk4
 
 def shootingMethod(p,q,r,a,b,alfa,beta,n):
 
-    # n points
+    # n points -> n-1 sub intervals
     
     h = (b-a)/(n-1)
     u = np.zeros([2,n+1])
     v = np.zeros([2,n+1])
+    v[1,0] = 1
     x = np.linspace(a,b,n)
 
     for i in range(0,n-1):
@@ -26,11 +28,18 @@ def shootingMethod(p,q,r,a,b,alfa,beta,n):
     w = np.zeros([2,n+1])
 
     for i in range(1,n-1):
-        w[1,i] = u[1,i] + w[2,0]*v[1,i]
-        w[2,i] = u[2,i] + w[2,0]*v[2,i]
+        w[0,i] = u[0,i] + w[1,0]*v[0,i]
+        w[1,i] = u[1,i] + w[1,0]*v[1,i]
         x = a+i*h
 
     return (x,w)
+
+def fun1(x, y, z):
+
+    
+def fun2(x, y, z):
+
+    
 
 def main():
 
