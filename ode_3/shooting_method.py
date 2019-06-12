2286e77dded77e9d38b36a2ec98ff3959d1e64c9
unknown <nil_al_7@hotmail.com>
2019-06-12 13:00:47 -0500

    Shooting method and module

--- ode_3/shooting_method.py
+++ ode_3/shooting_method.py
@@ -6,46 +6,86 @@
          q(x)
          r(x)
          a,b
-         alfa,beta 
+         alpha,beta 
 
 # OUTPUT : approximations w1,i to y(xi)
                           w2,i to y'(xi)  to all i = 0, ... ,N
 """
 
 import numpy as np
-import rungekutta4-system as rk4
+import rungekutta4mod as rk4
+import matplotlib.pyplot as plt
 
-def shootingMethod(p,q,r,a,b,alfa,beta,n):
+def shootingMethod(a,b,alpha,beta,n):
 
-    # n points -> n-1 sub intervals
+    # n subintervals
     
-    h = (b-a)/(n-1)
+    h = (b-a)/n
     u = np.zeros([2,n+1])
     v = np.zeros([2,n+1])
-    v[1,0] = 1
-    x = np.linspace(a,b,n)
+    x = np.linspace(a,b,n+1)
 
-    for i in range(0,n-1):
-        #Apply Runge -  Kutta to u1,i+1  u2,i+1  v1,i+1  v2,i+1
-
+    #Apply Runge -  Kutta to u1,i+1  u2,i+1  v1,i+1  v2,i+1
+    u = rk4.solveSystem(fun1,fun2,a,alpha,0,b,n+1)
+    v = rk4.solveSystem(fun1,fun2,a,0,1,b,n+1)
+                        
     w = np.zeros([2,n+1])
-
-    for i in range(1,n-1):
-        w[0,i] = u[0,i] + w[1,0]*v[0,i]
-        w[1,i] = u[1,i] + w[1,0]*v[1,i]
-        x = a+i*h
+    w[0,0] = alpha
+    w[1,0] = (beta-u[0][0,n])/v[0][0,n]
+    
+    for i in range(1,n):
+        w[0,i] = u[0][0,i] + w[1,0]*v[0][0,i]
+        w[1,i] = u[1][0,i] + w[1,0]*v[1][0,i]
 
     return(x,w)
 
-def fun1(x, y, z):
 
-    
-def fun2(x, y, z):
+"""
+u'' = p(x)u' + q(x)u + r(x)
 
-    
+u'  = n  -> n(a)  = alpha
+u'' = n' -> n'(a) = 0
+
+"""
+def fun1(x, u, n):
+    return n
+
+"""
+v'' = p(x)u' + q(x)u + r(x)
+
+v'  = n  -> n(a)  = 0
+v'' = n' -> n'(a) = 1
+"""
+def fun2(x, v, n):
+    return p(x)*n + q(x)*v + r(x)
+
+
+# Modify p(x) q(x) and r(x) to use.
+
+def p(x):
+    return 0
+
+def q(x):
+    return -4
+
+def r(x):
+    return 0
 
 def main():
 
-    shootingMethod() # To use in general
+    a = 0.0
+    b = np.pi
+    alpha = -2.0
+    beta = 10.0
+    n = 40
+    x,w = shootingMethod(a,b,alpha,beta,n) # To use in general
+
+    plt.plot(x,w[0],label='W_0')
+    plt.plot(x,w[1],label='W_1')
+    plt.xlabel("X values")
+    plt.ylabel("Y values")
+    plt.legend()
+    plt.title("Solution with shooting method")
+    plt.show()
     
 main()
