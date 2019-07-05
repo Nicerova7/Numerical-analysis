8ef8f99a382664ee78972f3f61c189c304a2fec2
unknown <nil_al_7@hotmail.com>
2019-07-05 18:10:13 -0500

    Forward difference to heat eq

--- pde_4/heatfd.m
+++ pde_4/heatfd.m
@@ -1,21 +1,44 @@
-% We are going to use heatfd(0,1,0,1,10,250)
+% Forward difference method to solve heat equation
 
-function w = heatfd(xl,xr,yb,yt,M,N)
+%{  
+    du(x,t)/dt = D*d^2u(x,t)/dx^2
+    
+    a < x < b
+    t1 < t < t2
+
+    Boundary Conditions
+
+    u(x,0) = f(x) 
+    u(a,t) = l(t)
+    u(b,t) = r(t)
+
+INPUT: a,b
+       t1,t2
+       D -> Diffusion coefficient
+       M -> number of steps in x direction
+       N -> number of steps in t direction
+
+%}
+
+% w = heatfd(D,a,b,t1,t2,M,N) 
+% We are going to use heatfd(1,0,1,0,1,10,250)
+
+function w = heatfd(D,xl,xr,yb,yt,M,N)
 
 f = @(x) sin(2*pi*x).^2;
 r = @(t) 0*t;
 l = @(t) 0*t;
 
-D = 1;
+
 h = (xl-xr)/M; k = (yt-yb)/N; 
 m = M-1 ; n = N;
 
 lambda = D*k/(h*h);
 a = diag(1-2*lambda*ones(m,1)) + diag(lambda*ones(m-1,1),1);
-a = a + diag(lambda*ones(m-1,1),-1);
+a = a + diag(lambda*ones(m-1,1),-1);  % define A matrix
 
-lside = l(yb+(0:n)*k);
-rside = l(yb+(0:n)*k);
+lside = l(yb+(0:n)*k); % define left side
+rside = l(yb+(0:n)*k); % define rigth side
 w(:,1) = f(xl+(1:m)*h)';
 
 for j =1:n
