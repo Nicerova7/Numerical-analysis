422b3d62f137d22622c4f7432b8aa918aae14b51
unknown <nil_al_7@hotmail.com>
2019-07-06 12:30:55 -0500

    backward method with Neumann cond

--- pde_4/heatbdneumann.m
+++ pde_4/heatbdneumann.m
@@ -1,4 +1,5 @@
 % Backward difference method to solve heat equation
+% with Neumann conditions
 
 %{  
     du(x,t)/dt = D*d^2u(x,t)/dx^2
@@ -30,22 +31,24 @@ function w = heatbdneumann(D,xl,xr,yb,yt,M,N)
 f = @(x) sin(2*pi*x).^2; 
 
 h = (xr-xl)/M; k = (yt-yb)/N; 
-m = M+1 ; n = N;
+m = M+1 ; % add two values ( dirichlet is M-1 )
+n = N;
 
 lambda = D*k/(h*h);
 a = diag(1+2*lambda*ones(m,1)) + diag(-lambda*ones(m-1,1),1);
 a = a + diag(-lambda*ones(m-1,1),-1);  % define A matrix
 
 % Neumann conditions
-a(1,:) = [-3 4 -1 zeros(1,m-3)]; % coeficients to know w0 and wM
+% Thanks to two extra values (M+1) we can put this coefficients
+a(1,:) = [-3 4 -1 zeros(1,m-3)]; 
 a(m,:) = [zeros(1,m-3) -1 4 -3];
 
 w(:,1) = f(xl+(0:M)*h)'; % we need to know w0 and wM, two extra values
 
 for j =1:n
     b = w(:,j); 
-    b(1) = 0; % we don't know w0 and wM
-    b(m) = 0;
+    b(1) = 0; % thanks to approximate of first derivate 
+    b(m) = 0; % and the homogeneus conditions both are zeros.
     w(:,j+1) = a\b; % w/a
 end
 
