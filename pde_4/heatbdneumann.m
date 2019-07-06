0ee69eeed81d1db216515dc0e028edc8cb8a282b
unknown <nil_al_7@hotmail.com>
2019-07-05 23:59:10 -0500

    Backward with Neumann conditions

---
+++ pde_4/heatbdneumann.m
@@ -0,0 +1,60 @@
+% Forward difference method to solve heat equation
+
+%{  
+    du(x,t)/dt = D*d^2u(x,t)/dx^2
+    
+    a < x < b
+    t1 < t < t2
+
+    Boundary Conditions
+
+    u(x,0) = f(x) 
+    du(a,t)/dx = 0
+    du(b,t)/dx = 0
+
+INPUT: a,b
+       t1,t2
+       D -> Diffusion coefficient
+       M -> number of steps in x direction
+       N -> number of steps in t direction
+
+%}
+
+% w = heatfd(D,a,b,t1,t2,M,N) 
+% We are going to use heatfd(1,0,1,0,1,20,20)
+% N = 20 undiconditionally stable
+
+function w = heatbdneumann(D,xl,xr,yb,yt,M,N)
+
+%modify to use
+f = @(x) sin(2*pi*x).^2; 
+
+h = (xr-xl)/M; k = (yt-yb)/N; 
+m = M+1 ; n = N;
+
+lambda = D*k/(h*h);
+a = diag(1+2*lambda*ones(m,1)) + diag(-lambda*ones(m-1,1),1);
+a = a + diag(-lambda*ones(m-1,1),-1);  % define A matrix
+
+% Neumann conditions
+a(1,:) = [-3 4 -1 zeros(1,m-3)]; % coeficients to know w0 and wM
+a(m,:) = [zeros(1,m-3) -1 4 -3];
+
+w(:,1) = f(xl+(0:M)*h)'; % we need to know w0 and wM, two extra values
+
+for j =1:n
+    b = w(:,j); 
+    b(1) = 0; % we don't know w0 and wM
+    b(m) = 0;
+    w(:,j+1) = a\b; % w/a
+end
+
+x = (0:M)*h;
+t = (0:n)*k;
+
+mesh(x,t,w')
+view(60,30);
+axis([xl xr yb yt -1 1]);
+
+
+
